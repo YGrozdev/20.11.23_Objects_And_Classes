b779f4eedf507c95d37993f82ca9d0c1cb826089
Yordan Grozdev <grozdev.yg@gmail.com>
2023-11-25 12:17:08 +0200

    Add project files.

---
+++ 05.Demo/Program.cs
@@ -0,0 +1,41 @@
+﻿using _00.Demos;
+using _05.Demo;
+
+//Dog puppy = new Dog
+//{
+//    Name = "Sparky",
+//    Breed = "Corgi",
+//    Age = 3,
+//};
+
+//puppy.Bark();
+
+//Dog friend = new Dog();
+//friend.Name = "Ivan";
+//friend.Breed = "Unknown";
+//friend.Age = 3;
+
+//friend.Bark();
+
+Dog dog1 = new Dog();
+Dog dog = new Dog("John", "Corgi");
+Dog dog2 = new Dog("John", "Corgi1", 12);
+
+Console.WriteLine(dog2.Name);
+
+
+//Dog test = null;
+
+//Console.WriteLine(test.Name);
+
+
+// PascalCase
+// camelCase
+// kebab-case
+// underscore_case
+// ALL_UPPER_CASE
+
+
+Random dice = new Random();
+int roll1 = dice.Next(1, 6);
+Console.WriteLine($"First roll: {roll1}");
